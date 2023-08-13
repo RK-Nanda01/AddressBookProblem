0a41edd18b6dec8d28cb8058cfb9c576d23a83b6
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-08-13 16:14:42 +0530

    [RK Nanda] Creating Contact class to create contacts

---
+++ AddressBookProblem/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace AddressBookProblem
+{
+	public class Contact
+	{
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string state;
+        private string city;
+        private int zipCode;
+        private long phoneNumber;
+        private string emailId;
+
+        public Contact(string firstName, string lastName, string address, string state, string city, string email, int zipCode, long phoneNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.state = state;
+            this.city = city;
+            this.emailId = email;
+            this.zipCode = zipCode;
+            this.phoneNumber = phoneNumber;
+        }
+    }
+}
+
